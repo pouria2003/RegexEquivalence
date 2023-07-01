3dfdfc5ee77563cf68d55cf43fa0ba98d71fe9af
Aminmdge <m.amin.h.2646@gmail.com>
2023-07-01 18:09:07 +0330

    regext To NFA function initialized

--- NFA.cpp
+++ NFA.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "NFA.h"
+
+NFA NFA::regexToNFA(const string & regex, int p, int q) {
+    if(findMatchingRightParentheses(regex, p) == q)
+        return regexToNFA(regex, p + 1, q - 1);
+    else if() {
+
+    }
+}
+
+NFA NFA::operator+(const NFA &other) {
+    return NFA();
+}
--- NFA.h
+++ NFA.h
@@ -4,9 +4,28 @@
 
 #ifndef REGEX_EQUIVALENCE_NFA_H
 #define REGEX_EQUIVALENCE_NFA_H
+#include <iostream>
+#include "main.cpp"
+#include "State.h"
+#include <list>
 
+
+using namespace std;
 
 class NFA {
+private:
+    char* alphabet;
+    int alphabet_number;
+    int state_number;
+    list <int>** transition_matrix;
+
+
+
+public:
+    static NFA regexToNFA(const string & regex, int p, int q);
+    NFA operator+ (const NFA & other);
+    NFA operator* (const NFA & other);
+    void addTransitoin(int state, char word, int destination);
 
 };
 
